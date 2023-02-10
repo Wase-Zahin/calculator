cdf21be4a36ea813acb6cc1aeab5bdd7f52ba4ce
Wase-Zahin <w.zahin@yahoo.com>
2023-02-10 17:52:55 +0800

    code update

--- js.js
+++ js.js
@@ -1,229 +1,120 @@
-const buttons_container = document.querySelector('.buttons-container');
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
 
-// create all buttons //
-for (let i = 0; i < 19; i++) {
-    let button = document.createElement('p');
-    if (i == 16) {
-        let zero = document.createElement('div');
-        zero.classList.add('zero_container');
-        buttons_container.appendChild(zero);
-        zero.appendChild(button);
-        button.classList.add('button');
-        button.classList.add('zero');
-    }
+let firstValue = null;
+let operator = null;
+let awaitingNextValue = false;
 
-    else {
-        button.classList.add('button');
-        buttons_container.appendChild(button);
-    };
-    switch (i) {
-        case 0:
-            button.textContent = 'AC';
-            break;
-        case 1:
-            button.textContent = 'X';
-            button.style.color = '#E83A14';
-            break;
-        case 2:
-            button.textContent = '+/-';
-            break;
-        case 3:
-            button.textContent = '/';
-            break;
-        case 4:
-            button.textContent = 7;
-            button.style.background = '#6A67CE'
-            break;
-        case 5:
-            button.textContent = 8;
-            button.style.background = '#6A67CE'
-            break;
-        case 6:
-            button.textContent = 9;
-            button.style.background = '#6A67CE'
-            break;
-        case 7:
-            button.textContent = '*';
-            break;
-        case 8:
-            button.textContent = 4;
-            button.style.background = '#6A67CE'
-            break;
-        case 9:
-            button.textContent = 5;
-            button.style.background = '#6A67CE'
-            break;
-        case 10:
-            button.textContent = 6;
-            button.style.background = '#6A67CE'
-            break;
-        case 11:
-            button.textContent = '-';
-            break;
-        case 12:
-            button.textContent = 1;
-            button.style.background = '#6A67CE'
-            break;
-        case 13:
-            button.textContent = 2;
-            button.style.background = '#6A67CE'
-            break;
-        case 14:
-            button.textContent = 3;
-            button.style.background = '#6A67CE'
-            break;
-        case 15:
-            button.textContent = '+';
-            break;
-        case 16:
-            button.textContent = 0;
-            button.style.background = '#6A67CE'
-            break;
-        case 17:
-            button.textContent = '.';
-            break;
-        case 18:
-            button.textContent = '=';
-            button.style.background = '#00C897';
-    }
-
-}
-
-const result = document.querySelector('.result');
-const buttons = document.querySelectorAll('.button');
-
-// variables and array used for the calculations
-// when clicked an operator store it in the array variable and later use it in the
-// switch case statements with the reduce functions to do the calculations.
-let arr = [];
-let operator = [];
-let numStr = '';
-let initialVal = 0; 
-
-
-// function to display calculator results
-function display(e) {
-    
-    // remove digits when clicked 'X'
-    if (numStr != '' && e.target.textContent == 'X') {
-        numStr = numStr.slice(0, -1);
-        result.textContent = result.textContent.slice(0, -1);
-        if (numStr == '') result.textContent = 0;
-    }
-
-    // add negative operator to a number.
-    if (e.target.textContent == '+/-' && result.textContent[0] != '-') {
-        numStr = '-' + numStr;
-        result.textContent = '-' + result.textContent;
-    };  
-
-    
-    // numStr becomes empty when an operator is invoked. So when numStr is empty
-    // remove the textcontent of results and display new numbers onclick.
-    if (numStr == '') result.textContent = '';
-    if (numStr.length < 8 && !isNaN(e.target.textContent) || e.target.textContent == '.' && !result.textContent.includes('.')) result.textContent += e.target.textContent;
-
-    // clear all the variables and array when clicked AC
-    if (e.target.textContent == 'AC') {
-        result.textContent = 0;
-        arr.length = 0;
-        operator=[];
-        result.textContent = 0;
-        numStr = '';
-    };
-    
-    if (result.textContent == '') result.textContent = 0;
-    if (numStr == '0' && e.target.textContent == '0') {
-        numStr = '';
-        result.textContent = '';
-    }
-    
-
+const performAddition = () => {
+  const secondValue = parseFloat(display.value);
+  display.value = firstValue + secondValue;
+  firstValue = null;
+  operator = null;
+  awaitingNextValue = false;
 };
 
-// the actual function that calculate the results
-function add(e) {
+const performSubtraction = () => {
+  const secondValue = parseFloat(display.value);
+  display.value = firstValue - secondValue;
+  firstValue = null;
+  operator = null;
+  awaitingNextValue = false;
+};
 
-    // numStr is used for pushing multiple digit numbers into array without
-    // breaking into individuals. Also when an operator is clicked empty the 
-    // value of numStr and push it to arr
-    if (e.target.textContent != 'AC' && e.target.textContent != 'X' && e.target.textContent != '+/-') {
-        if (numStr.length < 8 && !isNaN(e.target.textContent) || e.target.textContent == '.' && !numStr.includes('.') ) {
-            numStr += e.target.textContent;
-        };
+const performMultiplication = () => {
+  const secondValue = parseFloat(display.value);
+  display.value = firstValue * secondValue;
+  firstValue = null;
+  operator = null;
+  awaitingNextValue = false;
+};
 
-        if (isNaN(e.target.textContent) && e.target.textContent != '.') {
-            // when '=' operator is clicked
-            if (arr.length == 1 && operator.length == 0) {
-                operator.push(e.target.textContent);
-                numStr = '';
-            }
-            else { 
-                operator.push(e.target.textContent);
-                arr.push(Math.round(numStr * 10)/10);
-                numStr = '';
-            }
-        };
-    };
+const performDivision = () => {
+  const secondValue = parseFloat(display.value);
+  display.value = firstValue / secondValue;
+  firstValue = null;
+  operator = null;
+  awaitingNextValue = false;
+};
 
-    //reduce functions for calculations
-    let calcAdd = arr.reduce((prev, curr) => arr[0] + arr[1], initialVal);
-    let calcMin = arr.reduce((prev, curr) => arr[0] - arr[1], initialVal);
-    let calcProd = arr.reduce((prev, curr) => arr[0] * arr[1], initialVal);
-    let calcDiv = arr.reduce((prev, curr) => arr[0] / arr[1], initialVal);
+const performCalculation = () => {
+  if (operator === '+') {
+    performAddition();
+  } else if (operator === '-') {
+    performSubtraction();
+  } else if (operator === '*') {
+    performMultiplication();
+  } else if (operator === '÷') {
+    performDivision();
+  }
+};
 
-    // when there is 2 elements in array delete the first operator and calculate
-    // on that operator. 
-    if (arr.length >= 2) {
-        switch (operator[0]) {
-            case '+' || '=':
-                arr = [Math.round(calcAdd * 10) / 10];
-                operator.splice(0,1);
-                result.textContent = Math.round(calcAdd * 10) / 10;
-                break;
-            case '-' || '+/-' || '=':
-                arr = [Math.round(calcMin * 10) / 10];
-                operator.splice(0,1);
-                result.textContent = Math.round(calcMin * 10) / 10;
-                break;
-            case '*' || '=':
-                arr = [Math.round(calcProd * 10) / 10];
-                operator.splice(0,1);
-                result.textContent = Math.round(calcProd * 10) / 10;
-                break;
-            case '/' || '=':
-                arr = [Math.round(calcDiv * 10) / 10];
-                operator.splice(0,1);
-                result.textContent = Math.round(calcDiv * 10) / 10;
-                break;
+const handleValue = (value) => {
+  if (display.value.length >= 10) {
+    return;
+  }
+
+  if (awaitingNextValue) {
+    clearDisplay();
+    awaitingNextValue = false;
+  }
+
+  if (value === '.') {
+    if (display.value.includes('.')) {
+      return;
+    }
+  }
+
+  display.value += value;
+};
+const handleOperator = (value) => {
+  if (firstValue === null) {
+    firstValue = parseFloat(display.value);
+  }
+
+  if (operator !== null && display.value === '') {
+    display.value = '0';
+    return;
+  }
+
+  operator = value;
+  display.value = '';
+  awaitingNextValue = true;
+};
+
+const clearDisplay = () => {
+  display.value = '';
+};
+
+const removeLastDigit = () => {
+  display.value = display.value.slice(0, -1);
+};
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const value = e.target.innerText;
+  
+      if (value === 'C') {
+        clearDisplay();
+        firstValue = null;
+        operator = null;
+        awaitingNextValue = false;
+      } else if (value === 'CE') {
+        clearDisplay();
+      } else if (value === '+' || value === '-' || value === '*' || value === '÷') {
+        if (operator) {
+          display.value = '0';
         }
-    }
-
-    // if the operator array contains '=' immediately delete it because the
-    // reduce functions can't use it to do calculations and errors will occur.
-    if (operator.includes('=')) operator.splice(operator.indexOf('='),1);
-    if (arr.includes(NaN)) {
-        result.textContent = 0;
-        arr.length = 0;
-        operator=[];
-        result.textContent = 0;
-        numStr = '';
-    }
-    
-    // clear all variables and show error message if number length exceeds 9!
-    if (arr.length > 0 && arr[0].toString().length > 9) {
-        result.textContent = 'Error!'
-        arr.length = 0;
-        operator=[];
-        result.textContent = 0;
-        numStr = '';
-        
-    };
-    console.log(numStr);
-    console.log(operator);
-    console.log(arr);
-}
-
-// eventlisteners for buttons
-buttons.forEach((buttons) => buttons.addEventListener('click', display));
-buttons.forEach((buttons) => buttons.addEventListener('click', add));
+        handleOperator(value);
+      } else if (value === '=') {
+        performCalculation();
+        if (display.value.length > 10) {
+          display.value = parseFloat(display.value).toPrecision(10);
+        }
+      } else if (value === 'X') {
+        display.value = display.value.slice(0, -1);
+      } else {
+        handleValue(value);
+      }
+    });
+  });
